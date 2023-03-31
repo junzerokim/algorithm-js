7ad381674016199ea3b791c4ffc33c030436c039
Cos Kim <junzerokim@gmail.com>
2023-03-31 16:52:49 +0900

    Add: Algorithm

---
+++ JS/LV1/수박수박수박수박수박수?.js
@@ -0,0 +1,8 @@
+// 길이가 n이고, "수박수박수박수...."와 같은 패턴을 유지하는 문자열을 리턴하는 함수, solution을 완성하세요.
+// 예를들어 n이 4이면 "수박수박"을 리턴하고 3이라면 "수박수"를 리턴하면 됩니다.
+
+function solution(n) {
+  var answer = [];
+  for (i = 0; i < n; i++) answer[answer.length - 1] === '수' ? answer.push('박') : answer.push('수');
+  return answer.join('');
+}
