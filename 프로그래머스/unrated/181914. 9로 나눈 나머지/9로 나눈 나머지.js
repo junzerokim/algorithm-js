5c55ee7974731b85ee8c8997e570acbff468df61
Junyoung <72692386+junzerokim@users.noreply.github.com>
2024-01-08 09:03:13 +0900

    [level unrated] Title: 9로 나눈 나머지, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181914. 9로 나눈 나머지/9로 나눈 나머지.js
@@ -0,0 +1,11 @@
+function solution(number) {
+    var answer = 0;
+    let sum = 0;
+    
+    for (let i = 0; i < number.length; i++) {
+        sum += Number(number[i]);
+    }
+    
+    answer = sum % 9;
+    return answer;
+}
